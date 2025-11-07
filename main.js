119926ebad5a98bfd533271330707222534862c8
Gkhdegvb <gkhdegvb@gmail.com>
2025-11-07 17:52:56 +0100

    Optimization

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 window.addEventListener('load', function() {
     const up = document.querySelector('.up');
     const fadeElements = document.querySelectorAll('.fade');
-
     up.classList.add('animate');
-
     up.addEventListener('transitionend', function() {
         fadeElements.forEach(function(el) {
         el.classList.add('animate');
