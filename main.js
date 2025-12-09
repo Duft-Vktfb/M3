1a77436e0f0f377c3a44366b1b655d2ef25461bc
Gkhdegvb <gkhdegvb@gmail.com>
2025-12-09 08:41:27 +0100

    img listener

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 window.addEventListener('load', function() {
     const up = document.querySelector('.up');
     const fadeElements = document.querySelectorAll('.fade');
+
     up.classList.add('animate');
     up.addEventListener('transitionend', function() {
         fadeElements.forEach(function(el) {
-        el.classList.add('animate');
+            el.classList.add('animate');
+        });
+    });
+
+    const allImages = document.querySelectorAll('img');
+    let pressTimer;
+
+    allImages.forEach(function(img) {
+        img.addEventListener('contextmenu', function(e) {
+            e.preventDefault();
+        });
+        img.addEventListener('touchstart', function(e) {
+            pressTimer = setTimeout(function() {
+                e.preventDefault();
+            }, 400);
+        }, { passive: false });
+        img.addEventListener('touchend', function() {
+            clearTimeout(pressTimer);
         });
     });
 });
